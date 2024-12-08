1ab7e3afc1522ee8a030e7737816b3553156b07f
takumu <takumu155695@gmail.com>
2024-12-08 15:33:59 +0900

    ポインタ

--- variable_pointer/main.go
+++ variable_pointer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 const secret = "abc"
@@ -59,4 +60,35 @@ func main() {
 	fmt.Printf("i: %v %T\n", i, i)
 	i *= 2
 	fmt.Printf("i: %v %T\n", i, i)
+
+	var ui1 uint16
+	fmt.Printf("memory address of ui1: %p\n", &ui1)
+	var p1 *uint16
+	fmt.Printf("value of ui1: %v\n", p1)
+	p1 = &ui1
+	fmt.Printf("value of p1: %v\n", p1)
+	fmt.Printf("size of p1: %d[bytes]\n", unsafe.Sizeof(p1))
+	fmt.Printf("memory address of p1: %p\n", &p1)
+	fmt.Printf("value of p1(dereference): %v\n", *p1)
+	*p1 = 1
+	fmt.Printf("memory address of ui1: %v\n", ui1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1: %p\n", pp1)
+	fmt.Printf("memory address of pp1: %p\n", &pp1)
+	fmt.Printf("size of pp1: %d[bytes]\n", unsafe.Sizeof(pp1))
+	fmt.Printf("value of p1(dereference): %v\n", *pp1)
+	fmt.Printf("value of ui1(dereference): %v\n", **pp1)
+	**pp1 = 10
+	fmt.Printf("value of ui1: %v\n", ui1)
+
+	ok, result := true, "A"
+	if ok {
+		result := "B"
+		println(result)
+	} else {
+		result := "c"
+		println(result)
+	}
+	println(result)
 }
